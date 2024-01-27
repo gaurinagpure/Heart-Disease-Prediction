f382b67a8918f2e69d98e5f605278fbe2f3d7ba6
Your Name <you@example.com>
2024-01-27 17:59:00 +0530

    Update Flask app for JSON response

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pickle
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 
 # Load ML model
 model = pickle.load(open('model_saved', 'rb'))
@@ -14,26 +14,32 @@ def home():
     return render_template('Heart Disease Classifier.html')
 
 # Bind predict function to URL
-@app.route('/predict', methods =['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
+    try:
+        # Get the JSON data from the request
+        json_data = request.get_json()
 
-    # Put all form entries values in a list 
-    features = [float(i) for i in request.form.values()]
-    # Convert features to array
-    array_features = [np.array(features)]
-    # Predict features
-    prediction = model.predict(array_features)
+        # Convert JSON data to a list of numeric values
+        features = [float(json_data[key]) for key in json_data]
 
-    output = prediction
+        # Convert features to array
+        array_features = [np.array(features)]
 
-    # Check the output values and retrive the result with html tag based on the value
-    if output == 1:
-        return render_template('Heart Disease Classifier.html', 
-                               result = 'The patient is not likely to have heart disease!')
-    else:
-        return render_template('Heart Disease Classifier.html', 
-                               result = 'The patient is likely to have heart disease!')
+        # Predict features
+        prediction = model.predict(array_features)
+
+        # Additional information based on the prediction value
+        result_text = 'The patient is not likely to have heart disease!' if prediction[
+                                                                                0] == 1 else 'The patient is likely to have heart disease!'
+
+        # Return the prediction and additional information as JSON
+        return jsonify({"Result": int(prediction[0]), "ResultText": result_text})
+
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
-#Run the application
+    # Run the application
     app.run()
